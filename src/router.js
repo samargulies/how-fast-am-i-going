1c874be91366c55e86301bfa851aad36c670c919
Sam Margulies <smargulies@bicycletransit.com>
2019-07-26 19:46:27 -0400

    match scroll browser behavior

--- src/router.js
+++ src/router.js
@@ -50,6 +50,13 @@ const router = new Router({
       ],
     },
   ],
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 });
 
 router.beforeEach((to, from, next) => {
