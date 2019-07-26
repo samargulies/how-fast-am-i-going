45b3f0a9e3c58a0d317ac7b2937e96a27f87bd52
Sam Margulies <smargulies@bicycletransit.com>
2019-07-26 17:31:45 -0400

    flesh out api page styles

--- src/views/ApiPage.vue
+++ src/views/ApiPage.vue
@@ -1,20 +1,42 @@
 <template>
-  <div class="main">
-    <div class="section section--atlas">
-      <h1 class="section__title">What is my elevation: API</h1>
+  <div class="main page--api">
+    <div class="section">
+      <h1 class="section__title">What is my elevation <span class="api">API</span></h1>
 
-        <div class="section section--about">
-          <h2 class="section__title">Global Elevation data for a fraction of the price</h2>
-          <p>
-            This site was designed to help you find the elevation of your current location, or any point on Earth.
-            When viewed from a phone that supports elevation readings,
-            the reading will come directly from your device altitude reading and will update
-            automatically as you move. It will even work when your phone is offline.
-            From a computer your elevation is loaded from our API based on your location.
-          </p>
+        <div class="section section--api-intro">
+          <img src="/img/globe.svg" />
+          <h2 class="section__title">Global elevation data for a fraction of the price</h2>
+          <div class="text-block">
+            <p>As our traffic grew, we struggled to find a reliable source of global elevation data that fit within our budget. Our API now handles millions of elevation requests per month, and costs less than 1/10th what you would pay to Google.</p>
+            <p>Introducing the What is my elevation API.</p>
+          </div>
+          <a class="button" href="mailto:contact@whatismyelevation.com">Get in Touch</a>
         </div>
+
+        <div class="section section--features">
+          <div class="feature">
+            <h3 class="feature__title">Affordable</h3>
+            <img class="feature__image" src="/img/coin-stack.svg" />
+            <p class="feature__description">Built with budget in mind, our elevation API can handle millions of requests without breaking the bank.</p>
+          </div>
+          <div class="feature">
+            <h3 class="feature__title">Accurate</h3>
+            <img class="feature__image" src="/img/magnifying-glass.svg" />
+            <p class="feature__description">Aggregated from several open data providers, we offer <strong>30 meter global coverage</strong>, with up to 3 meter detail in the US and where data exists.</p>
+          </div>
+          <div class="feature">
+            <h3 class="feature__title">Scalable</h3>
+            <img class="feature__image" src="/img/chart.svg" />
+            <p class="feature__description">Built with budget in mind, our elevation API can handle millions of requests without breaking the bank.</p>
+        </div>
+        <div class="feature">
+          <h3 class="feature__title">Simple</h3>
+          <img class="feature__image" src="/img/browser-pin.svg" />
+          <p class="feature__description">Built with budget in mind, our elevation API can handle millions of requests without breaking the bank.</p>
+        </div>
+      </div>
     </div>
-    <TheFooter/>
+    <TheFooter :includeAbout="false" />
   </div>
 </template>
 
