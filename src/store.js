6337562baadfc5c46a16a09c349d405cee6f83ce
Sam Margulies <smargulies@bicycletransit.com>
2019-08-22 10:56:50 -0400

    Start slashing elevation codebase for new site

--- src/store.js
+++ src/store.js
@@ -1,35 +1,22 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import bingApi from '@/bingMaps';
-import customApi from '@/api';
-
-import config from '@/config';
-import { round } from '@/helpers';
-
-const api = config.ENABLE_CUSTOM_API ? customApi : bingApi;
-
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     loading: true,
-    setLocationOpen: false,
     location: {},
-    elevation: {},
     useFeet: true,
-    supportsElevation: false,
+    supportsLocation: false,
     watchId: null,
     colorScheme: localStorage.getItem('colorScheme') || 'auto',
   },
   actions: {
-    getUserLocation({ state, commit, dispatch }) {
+    getUserLocation({ commit, dispatch }) {
       const watchId = navigator.geolocation.watchPosition(
         (location) => {
-          // only update location if the watch is still active
-          if (state.watchId) {
-            dispatch('setUserLocation', location);
-          }
+          dispatch('setUserLocation', location);
         },
         error => dispatch('locationError', error),
         {
@@ -39,72 +26,21 @@ export default new Vuex.Store({
       );
       commit('setItem', { item: 'watchId', value: watchId });
     },
-    setUserLocation({ state, dispatch, commit }, location) {
-      const previousLocation = state.location;
-      dispatch('updateLocation', {
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        accuracy: location.coords.accuracy,
-      });
+    setUserLocation({ dispatch, commit }, location) {
+      commit('setItem', { item: 'location', value: location });
       if (location.coords.altitude) {
-        dispatch('setSupportsElevation', true);
-        commit('setElevation', {
-          elevation: location.coords.altitude,
-          source: 'phone',
-          accuracy: location.coords.altitudeAccuracy,
-        });
+        dispatch('setSupportsLocation', true);
         dispatch('setLoading', false);
-      } else if (state.location.latitude !== previousLocation.latitude
-        || state.location.longitude !== previousLocation.longitude) {
-        dispatch('fetchElevation');
       }
     },
     locationError({ commit, dispatch }, error) {
       console.warn('location access denied', error);
       commit('setItem', { item: 'loading', value: false });
-      dispatch('setLocationOpen', true);
+      dispatch('setSupportsLocation', false);
     },
     stopWatchingUserLocation({ state, commit }) {
       navigator.geolocation.clearWatch(state.watchId);
       commit('setItem', { item: 'watchId', value: null });
-    },
-    fetchElevation({ state, dispatch, commit }) {
-      if (!navigator.onLine) {
-        console.log('offline, not fetching elevation');
-        return false;
-      }
-      dispatch('stopWatchingUserLocation');
-      let loaded = false;
-      // delay showing the loading animation in case the response is super fast
-      setTimeout(() => {
-        if (!loaded) {
-          dispatch('setLoading', true);
-        }
-      }, 200);
-      return api.getElevation({
-        latitude: round(state.location.latitude, 5),
-        longitude: round(state.location.longitude, 5),
-      }).then((elevation) => {
-        loaded = true;
-        commit('setElevation', { elevation, source: 'web' });
-        dispatch('setLoading', false);
-      }).catch((error) => {
-        console.warn(error);
-        dispatch('setLocationOpen', true);
-      });
-    },
-    updateLocation({ commit }, {
-      latitude, longitude, accuracy = 100, title = null,
-    }) {
-      commit('setItem', {
-        item: 'location',
-        value: {
-          latitude, longitude, accuracy, title,
-        },
-      });
-    },
-    setLocationOpen({ commit }, isOpen) {
-      commit('setItem', { item: 'setLocationOpen', value: isOpen });
     },
     setLoading({ commit }, isLoading) {
       commit('setItem', { item: 'loading', value: isLoading });
@@ -112,8 +48,8 @@ export default new Vuex.Store({
     setUseFeet({ commit }, isFeet) {
       commit('setItem', { item: 'useFeet', value: isFeet });
     },
-    setSupportsElevation({ commit }, supportsElevation) {
-      commit('setItem', { item: 'supportsElevation', value: supportsElevation });
+    setSupportsLocation({ commit }, supportsLocation) {
+      commit('setItem', { item: 'supportsLocation', value: supportsLocation });
     },
     setColorScheme({ commit }, colorScheme) {
       localStorage.setItem('colorScheme', colorScheme);
