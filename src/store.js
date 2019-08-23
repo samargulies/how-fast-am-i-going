f1c3069a276ff5fa687052684eb112656c3f41bb
Sam Margulies <smargulies@bicycletransit.com>
2019-08-23 16:54:48 -0400

    test only running kalman for last 10 readings

--- src/store.js
+++ src/store.js
@@ -16,9 +16,9 @@ export default new Vuex.Store({
   },
   getters: {
     smoothedLocations(state) {
-      const filterLatitude = new KalmanFilter({ R: 0.001, Q: 0.0001 });
-      const filterLongitude = new KalmanFilter({ R: 0.001, Q: 0.0001 });
-      return state.locations.map(location => ({
+      const filterLatitude = new KalmanFilter();
+      const filterLongitude = new KalmanFilter();
+      return state.locations.slice(-10).map(location => ({
         latitude: filterLatitude.filter(location.coords.latitude),
         longitude: filterLongitude.filter(location.coords.longitude),
         timestamp: location.timestamp,
@@ -32,14 +32,14 @@ export default new Vuex.Store({
       }
       return 0;
     },
-    averageSpeed() {
+    averageSpeed(state, getters) {
       return 0;
-      // if (state.locations.length < 2) {
+      // if (getters.smoothedLocations.length < 2) {
       //   return 0;
       // }
       // let totalDuration = 0;
-      // const speeds = state.locations.reduce((a, b) => {
-      //   const duration = b.position.timestamp || 0 - a.position.timestamp || 0;
+      // const speeds = getters.smoothedLocations.reduce((a, b) => {
+      //   const duration = b.timestamp || 0 - a.timestamp || 0;
       //   totalDuration += duration;
       //   return (a.speed || 0 + b.speed || 0) * duration;
       // }, 0);
