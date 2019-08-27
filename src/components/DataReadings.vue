2115d3df059e9d459be437616d428c13f976f5cf
Sam Margulies <smargulies@bicycletransit.com>
2019-08-27 13:07:51 -0400

    add error message

--- src/components/DataReadings.vue
+++ src/components/DataReadings.vue
@@ -4,43 +4,44 @@
       <div class="double-bounce1"></div>
       <div class="double-bounce2"></div>
     </div>
-    <div v-else>
-      <div :class="['topline-readings', watchId ? 'watching' : 'not-watching']">
-        <div class="reading reading--avg-speed">
-          <div class="reading__value">
-            <i18n path="location-format" tag="div">
-              <span place="value">
-                {{ averageSpeed | numberFormatted({units, locale: $t.locale}) }}
-              </span>
-              <span place="units">{{ $t(`units.${units}`) }}</span>
-            </i18n>
-          </div>
-          <div class="reading__label">Average Speed</div>
-        </div>
-        <div class="reading reading--speed">
-          <div class="reading__value">
-            <i18n path="location-format" tag="div">
-              <span place="value">
-                {{ currentSpeed | numberFormatted({units, locale: $t.locale}) }}
-              </span>
-              <span place="units">{{ $t(`units.${units}`) }}</span>
-            </i18n>
-          </div>
-          <div class="reading__label">Current Speed</div>
-        </div>
-        <div class="reading">
-          <div class="reading__value" v-if="currentBearing !== null">
-            {{ currentBearing | bearingFormatted }}
-          </div>
-          <div class="reading__label">Bearing</div>
-        </div>
-      </div>
-      <!-- <div class="metadata">
-        <div class="watching-indicator" v-if="watchId">
-          Watching for updates
-        </div>
-      </div> -->
+    <div v-else-if="!supportsLocation" class="location-error">
+      <p>An error occurred loading your current location.</p>
     </div>
+    <div v-else :class="['topline-readings', watchId ? 'watching' : 'not-watching']">
+      <div class="reading reading--avg-speed">
+        <div class="reading__value">
+          <i18n path="location-format" tag="div">
+            <span place="value">
+              {{ averageSpeed | numberFormatted({units, locale: $t.locale}) }}
+            </span>
+            <span place="units">{{ $t(`units.${units}`) }}</span>
+          </i18n>
+        </div>
+        <div class="reading__label">Average Speed</div>
+      </div>
+      <div class="reading reading--speed">
+        <div class="reading__value">
+          <i18n path="location-format" tag="div">
+            <span place="value">
+              {{ currentSpeed | numberFormatted({units, locale: $t.locale}) }}
+            </span>
+            <span place="units">{{ $t(`units.${units}`) }}</span>
+          </i18n>
+        </div>
+        <div class="reading__label">Current Speed</div>
+      </div>
+      <div class="reading">
+        <div class="reading__value" v-if="currentBearing !== null">
+          {{ currentBearing | bearingFormatted }}
+        </div>
+        <div class="reading__label">Bearing</div>
+      </div>
+    </div>
+    <!-- <div class="metadata">
+      <div class="watching-indicator" v-if="watchId">
+        Watching for updates
+      </div>
+    </div> -->
   </div>
 </template>
 <script>
@@ -53,7 +54,7 @@ export default {
     };
   },
   computed: {
-    ...mapState(['units', 'loading', 'watchId', 'locationHasSteadied']),
+    ...mapState(['units', 'loading', 'watchId', 'supportsLocation']),
     ...mapGetters(['currentBearing', 'currentSpeed', 'averageSpeed']),
   },
 };
