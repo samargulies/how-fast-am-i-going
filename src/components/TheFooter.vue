b35b68af584ddc165d1da64ed72fe13dbb23963d
Sam Margulies <smargulies@bicycletransit.com>
2020-12-14 22:21:28 -0500

    increase translation link touch target on mobile
    and move lower on the page

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,70 +1,88 @@
 <template>
-<!-- eslint-disable max-len -->
-<footer class="footer">
-  <div class="section section--localizations">
-    <ul class="languages">
-      <li><router-link :to="pathForLocale({locale:'de'})">Deutsch</router-link></li>
-      <li><router-link :to="pathForLocale({locale:'es'})">Español</router-link></li>
-      <li><router-link :to="pathForLocale({locale:'en'})">English</router-link></li>
-      <li><router-link :to="pathForLocale({locale:'fr'})">Français</router-link></li>
-      <li><router-link :to="pathForLocale({locale:'it'})">Italiano</router-link></li>
-      <li><router-link :to="pathForLocale({locale:'pt'})">Português</router-link></li>
-      <li><router-link :to="pathForLocale({locale:'ru'})">Русский</router-link></li>
-      <li><router-link :to="pathForLocale({locale:'ja'})">日本語</router-link></li>
-      <li><router-link :to="pathForLocale({locale: 'zh'})">中文</router-link></li>
-    </ul>
-  </div>
-  <!-- Ezoic - Below Localizations - long_content -->
-  <Ezoic id="105" />
-  <!-- End Ezoic - Below Localizations - long_content -->
-  <slot>
-    <div class="section section--about" v-if="page">
-      <h2 class="section__title">{{ $t(`page.${page}.heading`) }}</h2>
-      <div class="text-block">
-        <p>{{ $t(`page.${page}.text`)}}</p>
+  <!-- eslint-disable max-len -->
+  <footer class="footer">
+    <!-- Ezoic - Below Localizations - long_content -->
+    <Ezoic id="105" />
+    <!-- End Ezoic - Below Localizations - long_content -->
+    <slot>
+      <div class="section section--about" v-if="page">
+        <h2 class="section__title">{{ $t(`page.${page}.heading`) }}</h2>
+        <div class="text-block">
+          <p>{{ $t(`page.${page}.text`)}}</p>
+        </div>
       </div>
-    </div>
-    <div class="section section--about" v-else-if="includeAbout">
-      <h2 class="section__title">{{ $t('what-is-this.heading') }}</h2>
-      <div class="text-block">
-        <p>{{ $t('what-is-this.p1') }}</p>
-        <h3>{{ $t('how-we-calc-speed.heading') }}</h3>
-        <p>{{ $t('how-we-calc-speed.p1') }}</p>
-        <!-- Ezoic - Below Description Paragraph 1 - longer_content -->
-        <Ezoic id="106" />
-        <!-- End Ezoic - Below Description Paragraph 1 - longer_content -->
-        <h3>{{ $t('graph-speed.heading') }}</h3>
-        <p>{{ $t('graph-speed.p1') }}</p>
-        <h3>{{ $t('suggested-uses.heading') }}</h3>
-        <p>{{ $t('suggested-uses.p1') }}</p>
-        <p>{{ $t('suggested-uses.p2') }}</p>
-        <p>{{ $t('suggested-uses.p3') }}</p>
-        <p>{{ $t('suggested-uses.p4') }}</p>
-        <p>{{ $t('suggested-uses.p5') }}</p>
-        <p>{{ $t('suggested-uses.p6') }}</p>
-        <p>{{ $t('suggested-uses.p7') }}</p>
-        <h3>{{ $t('current-speed.heading') }}</h3>
-        <p>{{ $t('current-speed.p1') }}</p>
-        <h3>{{ $t('avg-speed.heading') }}</h3>
-        <p>{{ $t('avg-speed.p1') }}</p>
-        <p>{{ $t('avg-speed.p2') }}</p>
-        <h3>{{ $t('supported-speed.heading') }}</h3>
-        <p>{{ $t('supported-speed.p1') }}</p>
-        <p>{{ $t('supported-speed.p2') }}</p>
+      <div class="section section--about" v-else-if="includeAbout">
+        <h2 class="section__title">{{ $t('what-is-this.heading') }}</h2>
+        <div class="text-block">
+          <p>{{ $t('what-is-this.p1') }}</p>
+          <h3>{{ $t('how-we-calc-speed.heading') }}</h3>
+          <p>{{ $t('how-we-calc-speed.p1') }}</p>
+          <!-- Ezoic - Below Description Paragraph 1 - longer_content -->
+          <Ezoic id="106" />
+          <!-- End Ezoic - Below Description Paragraph 1 - longer_content -->
+          <h3>{{ $t('graph-speed.heading') }}</h3>
+          <p>{{ $t('graph-speed.p1') }}</p>
+          <h3>{{ $t('suggested-uses.heading') }}</h3>
+          <p>{{ $t('suggested-uses.p1') }}</p>
+          <p>{{ $t('suggested-uses.p2') }}</p>
+          <p>{{ $t('suggested-uses.p3') }}</p>
+          <p>{{ $t('suggested-uses.p4') }}</p>
+          <p>{{ $t('suggested-uses.p5') }}</p>
+          <p>{{ $t('suggested-uses.p6') }}</p>
+          <p>{{ $t('suggested-uses.p7') }}</p>
+          <h3>{{ $t('current-speed.heading') }}</h3>
+          <p>{{ $t('current-speed.p1') }}</p>
+          <h3>{{ $t('avg-speed.heading') }}</h3>
+          <p>{{ $t('avg-speed.p1') }}</p>
+          <p>{{ $t('avg-speed.p2') }}</p>
+          <h3>{{ $t('supported-speed.heading') }}</h3>
+          <p>{{ $t('supported-speed.p1') }}</p>
+          <p>{{ $t('supported-speed.p2') }}</p>
+        </div>
       </div>
+    </slot>
+    <div class="section section--localizations">
+      <ul class="languages">
+        <li>
+          <router-link :to="pathForLocale({locale:'de'})">Deutsch</router-link>
+        </li>
+        <li>
+          <router-link :to="pathForLocale({locale:'es'})">Español</router-link>
+        </li>
+        <li>
+          <router-link :to="pathForLocale({locale:'en'})">English</router-link>
+        </li>
+        <li>
+          <router-link :to="pathForLocale({locale:'fr'})">Français</router-link>
+        </li>
+        <li>
+          <router-link :to="pathForLocale({locale:'it'})">Italiano</router-link>
+        </li>
+        <li>
+          <router-link :to="pathForLocale({locale:'pt'})">Português</router-link>
+        </li>
+        <li>
+          <router-link :to="pathForLocale({locale:'ru'})">Русский</router-link>
+        </li>
+        <li>
+          <router-link :to="pathForLocale({locale:'ja'})">日本語</router-link>
+        </li>
+        <li>
+          <router-link :to="pathForLocale({locale: 'zh'})">中文</router-link>
+        </li>
+      </ul>
     </div>
-  </slot>
-  <WhereWhat :fast="false" />
-  <div class="app-settings">
-    <a v-for="colorScheme in colorSchemes"
-      :key="colorScheme"
-      :class="['setting-button', `setting-button--color-scheme-${colorScheme}`,
+    <WhereWhat :fast="false" />
+    <div class="app-settings">
+      <a
+        v-for="colorScheme in colorSchemes"
+        :key="colorScheme"
+        :class="['setting-button', `setting-button--color-scheme-${colorScheme}`,
         currentColorScheme === colorScheme ? 'active' : '' ]"
-      @click="$store.dispatch('setColorScheme', colorScheme);">
-      {{ $t(`color-scheme-${colorScheme}`)}}
-    </a>
-  </div>
-</footer>
+        @click="$store.dispatch('setColorScheme', colorScheme);"
+      >{{ $t(`color-scheme-${colorScheme}`)}}</a>
+    </div>
+  </footer>
 </template>
 <script>
 import { pathForLocale } from '@/helpers';
